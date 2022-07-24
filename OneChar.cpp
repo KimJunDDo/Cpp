3d10b5daf4122050bf327241218fea6745dcee48
KimJunDDo <98204177+KimJunDDo@users.noreply.github.com>
2022-07-24 16:39:21 +0900

    P.91 10번

---
+++ OneChar.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+int main() {
+	char str[100];
+	cout << "문자열 입력>>";
+	cin.getline(str, 100, '\n');
+
+	for (int i = 0; i < strlen(str); i++) {
+		for (int j = 0; j < i + 1; j++) {
+			cout << str[j];
+		}
+		cout << endl;
+	}
+
+	return 0;
+}
