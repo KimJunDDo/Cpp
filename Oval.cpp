b35c948c5b4899630f52e461d1aba860d1c4952b
KimJunDDo <98204177+KimJunDDo@users.noreply.github.com>
2022-08-08 22:28:02 +0900

    P.153 9번

---
+++ Oval.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+using namespace std;
+
+class Oval {
+private:
+	int width;
+	int height;
+public:
+	Oval() {
+		width = 1;
+		height = 1;
+	}
+	Oval(int w, int h) {
+		this->width = w;
+		this->height = h;
+	}
+	~Oval() {
+		cout << "Oval 소멸 : width = " << width << ", height = " << height << endl;
+	}
+	int getWidth() {
+		return width;
+	}
+	int getHeight() {
+		return height;
+	}
+	void set(int w, int h) {
+		this->width = w;
+		this->height = h;
+	}
+	void show() {
+		cout << "width = " << width << ", height = " << height << endl;
+	}
+};
+int main() {
+	Oval a, b(3, 4);
+	a.set(10, 20);
+	a.show();
+	cout << b.getWidth() << "," << b.getHeight() << endl;
+
+	return 0;
+}
