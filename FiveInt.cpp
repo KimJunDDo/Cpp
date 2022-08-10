d5cd67325db034a4058f657df3f822915184c4b6
KimJunDDo <98204177+KimJunDDo@users.noreply.github.com>
2022-08-10 21:57:57 +0900

    P.211 2번

---
+++ FiveInt.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+	int* p = new int[5];
+	int sum = 0;
+	double avg;
+
+	cout << "정수 5개 입력>> ";
+	for (int i = 0; i < 5; i++) {
+		cin >> p[i];
+		sum += p[i];
+	}
+	avg = (double)sum / 5;
+	cout << "평균 " << avg;
+	delete[] p;
+
+	return 0;
+}
