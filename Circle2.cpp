5795d77299151dc5644cf5060250ee965fa11155
KimJunDDo <98204177+KimJunDDo@users.noreply.github.com>
2022-08-10 22:43:12 +0900

    P.213 8번

---
+++ Circle2.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+#define PI 3.141592
+using namespace std;
+
+class Circle {
+	int radius;
+public:
+	void setRadius(int radius);
+	double getArea();
+};
+inline void Circle::setRadius(int radius) {
+	this->radius = radius;
+}
+inline double Circle::getArea() {
+	return PI * radius * radius;
+}
+
+int main() {
+	cout << "원의 개수 >> ";
+	int n;
+	cin >> n;
+
+	if (n <= 0)
+		return 0;
+
+	Circle *pArray = new Circle[n];
+	int sum = 0;
+
+	for (int i = 0; i < n; i++) {
+		cout << "원 " << i + 1 << "의 반지름 >> ";
+		int radius;
+		cin >> radius;
+		(pArray+i) -> setRadius(radius);
+		if ((pArray+i) -> getArea() > 100)
+			sum++;
+	}
+	cout << "면적이 100보다 큰 원은 " << sum << "개 입니다";
+
+	delete[] pArray;
+}
