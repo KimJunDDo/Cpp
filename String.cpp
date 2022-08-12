a5b013c6ab27c68962ab779b4267966780d80896
KimJunDDo <98204177+KimJunDDo@users.noreply.github.com>
2022-08-12 15:16:41 +0900

    P.211 3번

---
+++ String.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+	string text;
+	int sum = 0;
+	int index = 0;
+
+	cout << "문자열 입력>> ";
+	getline(cin, text, '\n');
+
+	for (int i = 0; i < text.length(); i++) {
+		if (text.at(i) == 'a')
+			sum++;
+	}
+	cout << "문자 a는 " << sum << "개 있습니다." << endl;
+	cout << "문자 a의 인덱스 위치: ";
+
+	for (int i = index; i < text.length(); i++) {
+		int n = text.find('a', index);
+		if (n > index)
+			index = n + 1;
+		else if (n == -1)
+			break;
+		cout << n << ' ';
+	}
+
+	return 0;
+}
