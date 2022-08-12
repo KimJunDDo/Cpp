f39d92f66a18620f68ff3a01f28229bba46bf8c0
KimJunDDo <98204177+KimJunDDo@users.noreply.github.com>
2022-08-12 15:45:35 +0900

    P.212 5번

---
+++ RandomFixStr.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <string>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+int main() {
+	srand((unsigned)time(0));
+	string str;
+	cout << "아래에 한 줄을 입력하세요. (exit를 입력하면 종료합니다.)" << endl;
+
+	while (true) {
+		cout << ">>";
+		getline(cin, str, '\n');
+		if (str == "exit")
+			break;
+		int index = rand() % str.length();
+		char c = rand() % (122 - 97 + 1) + 97;
+		str[index] = c;
+		cout << str << endl;
+	}
+
+	return 0;
+}
