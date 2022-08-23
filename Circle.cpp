ef20087541d52010c8a0449ec88108e94c582dad
KimJunDDo <98204177+KimJunDDo@users.noreply.github.com>
2022-08-23 16:09:28 +0900

    P.270 5번

--- Circle.cpp
+++ Circle.cpp
@@ -1,33 +1,24 @@
 ﻿#include <iostream>
-#define PI 3.141592
 using namespace std;
 
 class Circle {
 	int radius;
 public:
-	void setRadius(int radius);
-	double getArea();
+	Circle(int r) { radius = r; }
+	int getRadius() { return radius; }
+	void setRadius(int r) { radius = r; }
+	void show() { cout << "반지름이 " << radius << "인 원" << endl; }
 };
-inline void Circle::setRadius(int radius) {
-	this->radius = radius;
-}
-inline double Circle::getArea() {
-	return PI * radius * radius;
+
+void increaseBy(Circle &a, Circle &b) {
+	int r = a.getRadius() + b.getRadius();
+	a.setRadius(r);
 }
 
 int main() {
-	Circle circleArray[3];
-	int sum = 0;
-
-	for (int i = 0; i < 3; i++) {
-		cout << "원 " << i + 1 << "의 반지름 >> ";
-		int radius;
-		cin >> radius;
-		circleArray[i].setRadius(radius);
-		if (circleArray[i].getArea() > 100)
-			sum++;
-	}
-	cout << "면적이 100보다 큰 원은 " << sum << "개 입니다";
+	Circle x(10), y(5);
+	increaseBy(x, y);
+	x.show();
 
 	return 0;
 }
