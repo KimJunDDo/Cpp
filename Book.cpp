4f9db14f0a11f2912cd48a804cfc36cd384f1153
KimJunDDo <98204177+KimJunDDo@users.noreply.github.com>
2022-08-28 16:40:40 +0900

    P.274 11번

---
+++ Book.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+class Book {
+	string name;
+	int price;
+public:
+	Book() {
+		name = " ";
+		price = 0;
+	}
+	Book(string name, int price) {
+		this->name = name;
+		this->price = price;
+	}
+	~Book() { };
+	void set(string name, int price) {
+		this->name = name;
+		this->price = price;
+	}
+	void show() {
+		cout << name << ' ' << price << "원" << endl;
+	}
+};
+
+int main() {
+	Book cpp("명품C++", 10000);
+	Book java = cpp;
+	java.set("명품자바", 12000);
+	cpp.show();
+	java.show();
+
+	return 0;
+}
