2f392216ac64f5ee84cce4691690030d6ef030b5
KimJunDDo <98204177+KimJunDDo@users.noreply.github.com>
2022-08-29 17:01:18 +0900

    P.314 4번

---
+++ MyVector.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+using namespace std;
+
+class MyVector {
+	int* mem;
+	int size;
+public:
+	MyVector(int n = 10, int val = 0) {
+		mem = new int[n];
+		size = n;
+		for (int i = 0; i < size; i++) {
+			mem[i] = val;
+			cout << mem[i] << ' ';
+		}
+		cout << endl;
+		cout << val << "이/가 " << n << "개 있습니다." << endl;
+	}
+	~MyVector() { delete[] mem; }
+};
+
+int main() {
+	MyVector x;
+	MyVector y(20, 8);
+
+	return 0;
+}
