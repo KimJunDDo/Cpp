6407913dfdb6291bc47973f0c45051412edaadc6
KimJunDDo <98204177+KimJunDDo@users.noreply.github.com>
2022-09-04 15:04:27 +0900

    P.367 2-1번

---
+++ Book4.cpp
@@ -0,0 +1,47 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+class Book {
+	string title;
+	int price, page;
+public:
+	Book(string title = " ", int price = 0, int page = 0) {
+		this->title = title;
+		this->price = price;
+		this->page = page;
+	}
+	void show() {
+		cout << title << " " << price << "원 " << page << " 페이지" << endl;
+	}
+	bool operator==(string title) {
+		if (this->title == title)
+			return true;
+		else
+			return false;
+	}
+	bool operator==(int price) {
+		if (this->price == price)
+			return true;
+		else
+			return false;
+	}
+	bool operator==(Book op2) {
+		if (this->title == op2.title && this->price == price && this->page == page)
+			return true;
+		else
+			return false;
+	}
+};
+
+int main() {
+	Book a("명품 C++", 30000, 500), b("고품 C++", 30000, 500);
+	if (a == 30000)
+		cout << "정가 30000원" << endl;
+	if (a == "명품 C++")
+		cout << "명품 C++ 입니다." << endl;
+	if (a == b)
+		cout << "두 책이 같은 책입니다." << endl;
+
+	return 0;
+}
