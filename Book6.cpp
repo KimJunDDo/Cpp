a92b4594099c0d89e01739d32087fbf7e7636a01
KimJunDDo <98204177+KimJunDDo@users.noreply.github.com>
2022-09-04 15:41:19 +0900

    P.367 3번

---
+++ Book6.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+class Book {
+	string title;
+	int price, page;
+public:
+	Book(string title, int price, int page) {
+		this->title = title;
+		this->price = price;
+		this->page = page;
+	}
+	bool operator!() {
+		if (this->price == 0)
+			return true;
+		else
+			return false;
+	}
+};
+int main() {
+	Book book("벼룩시장", 0, 50);
+	if (!book)
+		cout << "공짜다" << endl;
+
+	return 0;
+}
