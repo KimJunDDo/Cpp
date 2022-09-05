cac9bb19630855abb515da61fc6eaaf20e4ec18c
KimJunDDo <98204177+KimJunDDo@users.noreply.github.com>
2022-09-05 10:21:18 +0900

    P.369 8번

---
+++ CircleAbstract.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+using namespace std;
+
+class Circle {
+	int radius;
+public:
+	Circle(int radius = 0) { this->radius = radius; }
+	void show() { cout << "radius = " << radius << "인 원" << endl; }
+	friend Circle& operator++(Circle& op1);
+	friend Circle operator++(Circle &op1, int x);
+};
+
+Circle& operator++(Circle& op1) {
+	op1.radius++;
+	return op1;
+}
+Circle operator++(Circle& op1, int x) {
+	Circle tmp;
+	tmp.radius = op1.radius++;
+	return tmp;
+}
+
+int main() {
+	Circle a(5), b(4);
+	++a;
+	b = a++;
+	a.show();
+	b.show();
+
+	return 0;
+}
