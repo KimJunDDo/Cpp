607a272d425e18b931c36cc1cf26c899c9bd4bbd
KimJunDDo <98204177+KimJunDDo@users.noreply.github.com>
2022-09-05 14:06:28 +0900

    P.369 7번

---
+++ Matrix3.cpp
@@ -0,0 +1,44 @@
+﻿#include <iostream>
+using namespace std;
+
+class Matrix {
+	int m[4];
+public:
+	Matrix(int m1 = 0, int m2 = 0, int m3 = 0, int m4 = 0) {
+		m[0] = m1;
+		m[1] = m2;
+		m[2] = m3;
+		m[3] = m4;
+	}
+	void show() {
+		cout << "Matrix = { " << m[0] << " " << m[1] << " " << m[2] << " " << m[3] << " }" << endl;
+	}
+	friend int operator>>(Matrix op1, int x[]);
+	friend Matrix& operator<<(Matrix& op1, int y[]);
+};
+
+int operator>>(Matrix op1, int x[]) {
+	for (int i = 0; i < 4; i++) {
+		x[i] = op1.m[i];
+	}
+	return *x;
+}
+Matrix& operator<<(Matrix& op1, int y[]) {
+	for (int i = 0; i < 4; i++) {
+		op1.m[i] = y[i];
+	}
+	return op1;
+}
+
+int main() {
+	Matrix a(4, 3, 2, 1), b;
+	int x[4], y[4] = { 1, 2, 3, 4 };
+	a >> x;
+	b << y;
+
+	for (int i = 0; i < 4; i++) cout << x[i] << ' ';
+	cout << endl;
+	b.show();
+
+	return 0;
+}
